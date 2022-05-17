e8f7d9457aef9bfe65a98f4e93c03729492445dc
DevExpressExampleBot <fakeEmail>
2022-05-17 16:53:41 +0400

    Source auto update [skip ci]

--- VB/PrefilterFieldVsField/Form1.Designer.vb
+++ VB/PrefilterFieldVsField/Form1.Designer.vb
@@ -1,37 +1,37 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace PrefilterFieldVsField
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
-            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.[Default]
             Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
             Me.pivotGridControl1.Name = "pivotGridControl1"
@@ -39,24 +39,21 @@ Namespace PrefilterFieldVsField
             Me.pivotGridControl1.OptionsFilter.DefaultFilterEditorView = DevExpress.XtraEditors.FilterEditorViewMode.VisualAndText
             Me.pivotGridControl1.Size = New System.Drawing.Size(592, 382)
             Me.pivotGridControl1.TabIndex = 0
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(592, 382)
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Name = "Form1"
-            Me.Text = "How to define a filter criterion where two data fields are compared to each other" &
-    ""
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.Text = "How to define a filter criterion where two data fields are compared to each other" & ""
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
 #End Region
-
         Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-	End Class
+    End Class
 End Namespace
-
